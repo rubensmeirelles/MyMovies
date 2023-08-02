f719f5f5e04c04f31d9e6885916dadd708a514ff
rubensmeirelles <rrubens.meirelles@gmail.com>
2023-08-02 12:30:27 -0300

    Alteração do environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
-const apiKey = '6888da0641053ade917886ca8a7fdd72'
 export const environment = {
   production: true,
-  urlApi: `https://api.themoviedb.org/3${apiKey}`,
+  urlApi: `https://api.themoviedb.org/3`,
 };
